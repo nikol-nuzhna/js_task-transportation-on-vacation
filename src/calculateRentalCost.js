d4e354ec12b0dcd68ee19453abf39815f1d90179
Nikol Nuzhna <nika.nuzhna@gmail.com>
2025-03-31 14:47:16 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,25 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const price = days * 40;
+
+  if (days >= LONG_TERM) {
+    return price - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return price - SHORT_TERM_DISCOUNT;
+  }
+
+  if (days < 0) {
+    return 'Enter a valid number of days';
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
